608cae81e454478c3029ef9dc1716cc693027227
Domenico <domedfd@gmail.com>
2019-09-09 10:04:24 -0400

    Instalacion del plugin babel-plugin-transform-object-rest-spread para poder funcionar el spread

--- ex/index.js
+++ ex/index.js
@@ -1,4 +1,14 @@
-import Pessoa from "./pessoa";
+const produto = {
+  nome: "Caneta bic preta",
+  preco: 1.9,
+  desconto: 0.05
+};
 
-const pessoa = new Pessoa("Guilherme");
-console.log(pessoa.toString());
+function clone(objeto) {
+  return { ...objeto };
+}
+
+const novoProduto = clone(produto);
+novoProduto.nome = "Caneta bic azul";
+
+console.log(produto, novoProduto);
